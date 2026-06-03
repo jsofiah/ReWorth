5dcce4ca232474c93a44a414396d989dc9642766
Silmiuwww <nadiasalma078@gmail.com>
2026-06-03 22:07:11 +0700

    update laporan analitik & pdf

--- reworth_website/dlh/laporan_analitik.php
+++ reworth_website/dlh/laporan_analitik.php
@@ -88,8 +88,8 @@ function formatNumber($n)
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laporan dan Analitik - DLH ReWorth</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="stylesheet" href="style/root.css">
     <link rel="stylesheet" href="style/form.css">
 </head>
